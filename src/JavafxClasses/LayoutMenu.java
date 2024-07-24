a18c826aa676213bae1722389e5d51824f24aafc
Pujolras <jordipujolrasbosch@gmail.com>
2024-07-24 17:22:47 +0200

    gui v4

--- src/JavafxClasses/LayoutMenu.java
+++ src/JavafxClasses/LayoutMenu.java
@@ -3,12 +3,18 @@ package JavafxClasses;
 import Utils.Pair;
 import Utils.Utility;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,7 +71,8 @@ public class LayoutMenu {
     private static List<ItemMenu> buildMenu(){
         List<ItemMenu> list = new ArrayList<>();
         list.add(new ItemMenu("Information", null, false, false));
-        list.add(new ItemMenu("Welcome to the app", infoPane(), true, false));
+        list.add(new ItemMenu("Welcome to the app", startPane(), true, false));
+        list.add(new ItemMenu("Manuals", infoPane(), true, true));
         for(Pair<String,List<GuiMenu.GuiMenuIn>> pair : GuiMenu.getList()){
             list.add(new ItemMenu(pair.getA(), null, false, false));
             for(GuiMenu.GuiMenuIn g : pair.getB()){
@@ -75,11 +82,61 @@ public class LayoutMenu {
         return list;
     }
 
+    private static CallPane startPane(){
+        return () -> {
+            VBox layout = new VBox();
+            layout.setAlignment(Pos.CENTER);
+            layout.setPadding(new Insets(20,20,20,20));
+
+            Label title = new Label("Regular Languages &\nContext Free Languages\nTools");
+            title.setFont(Font.font("Source Code Pro", FontWeight.BOLD, 40));
+            title.setPadding(new Insets(20,20,20,20));
+            title.setTextAlignment(TextAlignment.CENTER);
+
+            layout.getChildren().addAll(title);
+            return layout;
+        };
+    }
+
     private static CallPane infoPane(){
         return () -> {
-            LayoutStart start = new LayoutStart();
-            return start.build();
+            VBox layout = new VBox();
+            layout.setAlignment(Pos.TOP_LEFT);
+            layout.setPadding(new Insets(20,20,20,20));
+
+            Label titleManual1 = new Label("Command line manual");
+            infoTitleProperties(titleManual1);
+
+            TextField manual1 = new TextField("https://github.com/JordiPujolrasBosch/FonamentsComputacio/blob/master/CommandLineManual.md");
+            infoManualProperties(manual1);
+
+            Label titleManual2 = new Label("Format of dfa, nfa, regex and cfg");
+            infoTitleProperties(titleManual2);
+
+            TextField manual2 = new TextField("https://github.com/JordiPujolrasBosch/FonamentsComputacio/blob/master/ReaderManual.md");
+            infoManualProperties(manual2);
+
+            Label title3 = new Label("Author");
+            infoTitleProperties(title3);
+
+            Label author = new Label("Made by: Jordi Pujolras Bosch");
+            author.setPadding(new Insets(0,0,0,10));
+
+            layout.getChildren().addAll(titleManual1, manual1, titleManual2, manual2, title3, author);
+            return layout;
         };
+    }
+
+    private static void infoTitleProperties(Label label){
+        label.setFont(Font.font("Source Code Pro", FontWeight.BOLD, 20));
+        label.setPadding(new Insets(20,0,20,10));
+    }
+
+    private static void infoManualProperties(TextField textField){
+        textField.setPadding(new Insets(0,0,0,10));
+        textField.setEditable(false);
+        textField.setMaxWidth(600);
+        textField.setPrefHeight(30);
     }
 
     private static class ItemMenu{
